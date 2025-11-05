b56497102aaa1a920a472075e620b59d0ab78267
princekhan7748 <princekhan7748@gmail.com>
2025-11-05 13:05:19 +0000

    feat: Initiation of Capaxcitor and starting android and IOS platform

---
+++ android/app/src/main/java/com/debateauditor/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.debateauditor.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.debateauditor.app',
+  appName: 'Debate Auditor',
+  webDir: 'out'
+};
+
+export default config;
